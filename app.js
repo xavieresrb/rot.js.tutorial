2d10906d9b7e13ca368cf2a64a5220f19edf76a2
xavieresrb <xavieres@gmail.com>
2018-01-26 20:26:32 +0100

    initial setup. part 1 of the tutorial completed.

---
+++ app.js
@@ -0,0 +1,45 @@
+var Game = {
+    map: {},
+    display: null,
+    init: function () {
+        this.display = new ROT.Display();
+        document.body.appendChild(this.display.getContainer());
+        this._generateMap();
+    },
+    _generateMap: function () {
+        var digger = new ROT.Map.Digger();
+        var freeCells = [];
+
+        var digCallback = function (x, y, value) {
+            if (value) { return; } /* do not store walls */
+
+            var key = x + "," + y;
+            this.map[key] = ".";
+            freeCells.push(key);
+        }
+        digger.create(digCallback.bind(this));
+
+        this._generateBoxes(freeCells);
+
+        this._drawWholeMap();
+    },
+    _drawWholeMap: function () {
+        for (var key in this.map) {
+            var parts = key.split(",");
+            var x = parseInt(parts[0]);
+            var y = parseInt(parts[1]);
+            this.display.draw(x, y, this.map[key]);
+        }
+    },
+    _generateBoxes: function(freeCells) {
+        for (var i=0;i<10;i++) {
+            var index = Math.floor(ROT.RNG.getUniform() * freeCells.length);
+            var key = freeCells.splice(index, 1)[0];
+            this.map[key] = "*";
+        }
+    }
+}
+
+
+
+Game.init();
